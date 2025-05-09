46313fad4ac07189213b5e28e30d91fe55fecba4
Carlos Petit <ca.alberto.p@gmail.com>
2025-05-09 18:21:11 -0400

    fix links

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -2,12 +2,12 @@
 export default new Map([
 ["src/content/blog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")],
 ["src/content/privacy/cahuinchile.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprivacy%2Fcahuinchile.mdx&astroContentModuleFlag=true")],
-["src/content/privacy/estimator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprivacy%2Festimator.mdx&astroContentModuleFlag=true")],
 ["src/content/privacy/honorarioscl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprivacy%2Fhonorarioscl.mdx&astroContentModuleFlag=true")],
-["src/content/support/estimator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsupport%2Festimator.mdx&astroContentModuleFlag=true")],
+["src/content/privacy/estimator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprivacy%2Festimator.mdx&astroContentModuleFlag=true")],
 ["src/content/support/cahuinchile.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsupport%2Fcahuinchile.mdx&astroContentModuleFlag=true")],
+["src/content/support/estimator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsupport%2Festimator.mdx&astroContentModuleFlag=true")],
 ["src/content/support/honorarioscl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fsupport%2Fhonorarioscl.mdx&astroContentModuleFlag=true")],
+["src/content/marketing/cahuinchile.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmarketing%2Fcahuinchile.mdx&astroContentModuleFlag=true")],
 ["src/content/marketing/estimator.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmarketing%2Festimator.mdx&astroContentModuleFlag=true")],
-["src/content/marketing/honorarioscl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmarketing%2Fhonorarioscl.mdx&astroContentModuleFlag=true")],
-["src/content/marketing/cahuinchile.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmarketing%2Fcahuinchile.mdx&astroContentModuleFlag=true")]]);
+["src/content/marketing/honorarioscl.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fmarketing%2Fhonorarioscl.mdx&astroContentModuleFlag=true")]]);
 		
